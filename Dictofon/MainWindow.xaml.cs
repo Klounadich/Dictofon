569d2f082e741aa2c27b123a2845234112973725
Klounadich <you@example.com>
2025-05-23 22:03:26 +0300

    Добавьте файлы проекта.

---
+++ Dictofon/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dictofon
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+        public void WindowMove (object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+        public void Close(object sender, MouseButtonEventArgs click)
+        {
+            Close();
+        }
+        public void Minimazed(object sender, MouseButtonEventArgs click)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+    }
+    
+    }
